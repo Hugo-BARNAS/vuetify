5407a7a90f5d75441107379ebda2f510eecd05fd
Hugo <hugo.barnas@gmail.com>
2020-11-02 17:17:15 +0100

    cards

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import responsive from "vue-responsive"
+Vue.use(responsive)
 
 Vue.config.productionTip = false;
 
 new Vue({
+  responsive,
   router,
   store,
   vuetify,
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -2,11 +2,44 @@
   <div class="home">
     <h1 class="subtitle-1 grey--text">Team</h1>
     <v-container class="my-5">
-        <p>content</p>
+      <v-layout row wrap> 
+        <v-flex xs12 sm6 md4 m4 lg3 v-for="person in team" :key="person.name">
+          <v-card flat class="text-sm-center ma-3">
+              <v-responsive class="pt-4">
+                images goes
+              </v-responsive>
+              <v-card-text>
+                <div class="subtitle-2">
+                  {{person.name}}
+                </div>
+                <div class="grey--text">
+                  {{person.role}}
+                </div> 
+              </v-card-text>
+              <v-card-actions>
+                <v-btn depressed >
+                  <v-icon small left color="grey">mdi-message</v-icon>
+                  <span class="grey--text">Message</span>
+                </v-btn>
+              </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      team: [
+        { name: "Hugo BARNAS", role: "Web devlopper" },
+        { name: "Cecile Delgnault", role: "UX Project Manager" },
+        { name: "Stephanie Michelon", role: "UI" },
+        { name: "Mickael Ribeiro", role: "Project Manager" },
+      ],
+    };
+  },
+};
 </script>
